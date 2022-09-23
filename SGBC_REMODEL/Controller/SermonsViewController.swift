7ad822577f8ec4a11876dbc6f8e6c4f67f6c58c1
temitayo65 <iloritemitayo65@gmail.com>
2022-09-23 11:13:51 +0100

    Include SermonByBooksTableView for books category

--- SGBC_REMODEL/Controller/SermonsViewController.swift
+++ SGBC_REMODEL/Controller/SermonsViewController.swift
@@ -13,6 +13,7 @@ class SermonsViewController: UIViewController, UITableViewDelegate, UITableViewD
     var sermons = [Sermons]()
     var currentIndexPath: Int = 0
     @IBOutlet var spinner: UIActivityIndicatorView!
+    var sermonByBookData = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1 Samuel", "2 Samuel","1 Kings", "2 Kings", "1 Chronicles", "2 Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclessiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentation", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zepheniah","Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2Timothy", "Titus", "Philemon", "Hebrew", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"]
    
 
     @IBOutlet var SermonsHeaderImageView: UIImageView!
@@ -45,6 +46,12 @@ class SermonsViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         SermonsHeaderImageView.image = UIImage(named: "sermonsimage")
         
+        
+        sermonByBooksTableView.register(UINib(nibName: "SermonByBooksTableViewCell", bundle: nil), forCellReuseIdentifier: "sermonbybooksidentifier")
+        sermonByBooksTableView.delegate = self
+        sermonByBooksTableView.dataSource = self
+        
+        
         recentSermonsTableView.register(UINib(nibName: "RecentSermonTableViewCell", bundle: nil), forCellReuseIdentifier: "recentsermonidentifier")
         recentSermonsTableView.delegate = self
         recentSermonsTableView.dataSource = self
@@ -61,6 +68,8 @@ class SermonsViewController: UIViewController, UITableViewDelegate, UITableViewD
         sermonManager.fetchSermons()
         
         recentSermonButton.backgroundColor = .blue
+        
+        sermonByBooksTableView.isHidden = true
         
     }
     
@@ -79,7 +88,10 @@ class SermonsViewController: UIViewController, UITableViewDelegate, UITableViewD
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sermons.count 
+        if tableView == recentSermonsTableView{return sermons.count}
+        else if tableView == sermonByBooksTableView{return sermonByBookData.count}
+        else{return 10}
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -88,14 +100,21 @@ class SermonsViewController: UIViewController, UITableViewDelegate, UITableViewD
         sermons.sort {sermon_one, sermon_two in
             sermon_one.date_preached > sermon_two.date_preached
         }
-        
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "recentsermonidentifier", for: indexPath) as? RecentSermonTableViewCell{
-            cell.RecentSermonTitleLabel.text = sermons[indexPath.row].title
-            let sliced_date = slice(sermons[indexPath.row].date_preached, from: 0, to: "T")!
-            cell.RecentSermonDate.text = sliced_date
-            cell.RecentSermonImageView.image = UIImage(named: "placeholder")
-            cell.RecentSermonPreacherName.text = sermons[indexPath.row].sermonPastor.first_name + " " + sermons[indexPath.row].sermonPastor.last_name
-            return cell 
+        if tableView == recentSermonsTableView{
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "recentsermonidentifier", for: indexPath) as? RecentSermonTableViewCell{
+                cell.RecentSermonTitleLabel.text = sermons[indexPath.row].title
+                let sliced_date = slice(sermons[indexPath.row].date_preached, from: 0, to: "T")!
+                cell.RecentSermonDate.text = sliced_date
+                cell.RecentSermonImageView.image = UIImage(named: "placeholder")
+                cell.RecentSermonPreacherName.text = sermons[indexPath.row].sermonPastor.first_name + " " + sermons[indexPath.row].sermonPastor.last_name
+                return cell
+            }
+        }
+        else if tableView == sermonByBooksTableView{
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "sermonbybooksidentifier", for: indexPath) as? SermonByBooksTableViewCell{
+                cell.bookTitle.text = sermonByBookData[indexPath.row]
+                return cell
+            }
         }
         return UITableViewCell()
     }
@@ -103,11 +122,12 @@ class SermonsViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         currentIndexPath = indexPath.row
         tableView.deselectRow(at: indexPath, animated: true) // remove selection background when clicked
-        performSegue(withIdentifier: "playAudio", sender: self)
+        if tableView == recentSermonsTableView{ performSegue(withIdentifier: "playAudio", sender: self)}
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 120
+        if tableView == recentSermonsTableView{return 120}
+        else{return 40}
     }
 
     
@@ -132,5 +152,29 @@ class SermonsViewController: UIViewController, UITableViewDelegate, UITableViewD
             }
         }
     }
+
+    
+    @IBOutlet var sermonByBooksTableView: UITableView!
+    
+    
+    
+    @IBAction func categoryButtonPressed(_ sender: UIButton) {
+        if sender.titleLabel?.text?.lowercased() == "books"{
+            recentSermonsTableView.isHidden = true
+            sermonByBooksTableView.isHidden = false
+            booksSermonButton.backgroundColor = .blue
+            recentSermonButton.backgroundColor = .clear
+        }
+        else if sender.titleLabel?.text?.lowercased() == "recent"{
+            recentSermonsTableView.isHidden = false
+            sermonByBooksTableView.isHidden = true
+            booksSermonButton.backgroundColor = .clear
+            recentSermonButton.backgroundColor = .blue
+        }
+        
+        
+    }
+    
+    
     
 }
---
+++ SGBC_REMODEL/View/SermonByBooksTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SermonByBooksTableViewCell.swift
+//  SGBC_REMODEL
+//
+//  Created by ADMIN on 23/09/2022.
+//
+
+import UIKit
+
+class SermonByBooksTableViewCell: UITableViewCell {
+    @IBOutlet var bookTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
